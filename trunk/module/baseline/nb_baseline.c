16d460d37b904074016a79d8b636230d4513e365
trettevik <trettevik@d8122d8b-3ab0-4c51-b7cd-e844dfa63d60>
2010-12-30 14:15:17 +0000

    baseline fix endless loop
    git-svn-id: http://svn.code.sf.net/p/nodebrain/code@47 d8122d8b-3ab0-4c51-b7cd-e844dfa63d60

--- trunk/module/baseline/nb_baseline.c
+++ trunk/module/baseline/nb_baseline.c
@@ -501,6 +501,7 @@ static void baselineAlert(nbCELL context,BTreeSkill *skillHandle,BTree *tree,BTr
   if(node->value<node->average) limit=node->average-node->threshold;
   else limit=node->average+node->threshold;
   node->level++;
+  if(node->threshold<=0) return; // make sure we don't loop here
   while((threshold=node->threshold*2) && deviation>threshold) node->level++,node->threshold=threshold;
   sprintf(cmdcur,"\",_value=%.10g,_average=%.10g,_sigma=%.10g,_deviation=%.10g,_threshold=%.10g,_limit=%.10g,_level=%d;",node->value,node->average,node->deviation*1.25,deviation,node->threshold,limit,node->level);
   nbActionCmd(context,cmd,0);
@@ -597,7 +598,7 @@ static void treeLoad(nbCELL context,void *skillHandle,BTree *tree){
   period=cycleTime/tree->interval;
   tree->period=period;
   sprintf(filename,"%s/%8.8d.nb",tree->directory,tree->period*tree->interval);
-  nbParseSource(context,filename); // load using the command interpreter
+  nbSource(context,filename); // load using the command interpreter
   }
 
 static void treeAlarm(nbCELL context,void *skillHandle,void *nodeHandle,nbCELL cell){
